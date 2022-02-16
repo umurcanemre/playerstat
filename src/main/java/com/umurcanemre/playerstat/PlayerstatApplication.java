bfa3a585087105e4e80fd503165fb79b92b4cd1c
umurcan emre <5785630+umurcanemre@users.noreply.github.com>
2022-02-16 22:03:55 +0100

    First commit, empty app

---
+++ src/main/java/com/umurcanemre/playerstat/PlayerstatApplication.java
@@ -0,0 +1,13 @@
+package com.umurcanemre.playerstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerstatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umurcanemre/playerstat/PlayerstatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umurcanemre.playerstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerstatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
